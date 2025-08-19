62fbd40622c364dceddda7668123ea7dd0571f94
almamuntmn <almamuncool88@gmail.com>
2025-08-19 12:59:29 +0600

    vite up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // if you're using React
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/knowledge-cafe-site/',
   plugins: [react()],
 })
